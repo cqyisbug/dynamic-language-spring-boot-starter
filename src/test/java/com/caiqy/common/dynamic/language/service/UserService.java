2c4af3a51dfc5935cc6a2f22c257a3db8af4d91b
caiqy <cqyisbug@163.com>
2021-09-08 18:14:47 +0800

    :tada: init project

--- src/test/java/com/caiqy/common/dynamic/language/service/UserService.java
+++ src/test/java/com/caiqy/common/dynamic/language/service/UserService.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright [2021] [caiqy]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.caiqy.common.dynamic.language.service;
 
 import com.caiqy.common.dynamic.language.annotation.GroovyDynamicLanguageService;
 
 /**
- * Created on 2021/6/24.
+ * 启用动态语言功能，从source中加载代码
  *
  * @author caiqy
  */
